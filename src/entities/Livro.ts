d368a41af42a3e3bf73f298333302f945c520b81
FernandesVasconcelos <fernandesvasconcelos2310@gmail.com>
2026-07-28 17:23:19 -0300

    feat: Configurar Atributo CapaUrl Livro.ts

--- src/entities/Livro.ts
+++ src/entities/Livro.ts
@@ -8,15 +8,17 @@ export class Livro {
     private _titulo!: string;
     private _autor!: string;
     private _disponivel!: boolean;
-    
+    private _capaUrl: string | null;
+
 // Constructor
 
-    constructor(id: string, titulo: string, autor: string, disponivel: boolean) {
+    constructor(id: string, titulo: string, autor: string, disponivel: boolean, capaUrl: string | null) {
         
         this._id = id;
         this.titulo = titulo;
         this.autor = autor;
         this._disponivel = disponivel;
+        this._capaUrl = capaUrl;
 
     }
 
@@ -56,6 +58,17 @@ export class Livro {
         this._disponivel = valor;
     } 
 
+    public get capaUrl(): string | null {
+        return this._capaUrl;
+    }
+
+    public set capaUrl(valor: string | null) {
+        if (valor !== null && valor.trim().length === 0) {
+            throw new Error("URL da capa inválida!")
+        }
+        this._capaUrl = valor ? valor.trim() : null;
+    }
+
 // Métodos
 
     public validar(): boolean {
@@ -71,7 +84,9 @@ export class Livro {
         if (this._disponivel === undefined || this._disponivel === null) {
             return false;
         }
-    
+        if (this._capaUrl !== null && this._capaUrl.trim().length === 0) {
+            return false;
+        }
         return true;
     }
 
@@ -80,7 +95,8 @@ export class Livro {
             id: this._id,
             titulo: this._titulo,
             autor: this._autor,
-            disponivel: this._disponivel
+            disponivel: this._disponivel,
+            capaUrl: this._capaUrl,
         };
     }
 
@@ -91,12 +107,12 @@ export class Livro {
 
         const statusDisponivel = dados.disponivel !== undefined ? Boolean(dados.disponivel) : true;
 
-
         return new Livro(
             dados.id,
             dados.titulo,
             dados.autor,
-            statusDisponivel
+            statusDisponivel !== undefined ? statusDisponivel : true,
+            dados.capaUrl,
         );
     }
 }
