ab2ae0e12a5fd9270854f6ae7d4b20d706c82d04
alice <batatinha127900@gmail.com>
2026-07-31 16:24:49 -0300

    feat: formulario de criação de livro

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import app from './app.js';
+import app from './app';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando em: http://localhost:${PORT}`);
+  console.log(`🚀 Servidor rodando em: http://localhost:${PORT}`);
 });
